bc876bcd73ab7b76d67d453a83d665ee743f8aca
Vishesh <visheshsingh16@gmail.com>
2017-08-16 21:42:15 -0400

    Lists, events and data binding added

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./market.component.css']
 })
 export class MarketComponent implements OnInit {
+    collectables = [
+        {description: 'A beginner\'s guide to jQuery', type: 'Book'},
+        {description: 'ReactJS: Make the BEST SPAs', type: 'Magazine'},
+        {description: 'a to z of JavaScript', type: 'Article'},
+        {description: 'Intermediate level: HTML/CSS', type: 'Periodical'}
+    ];
+
+    onAddCollection(){
+        console.log("Test!");
+    }
 
   constructor() { }
 
